ac45b1fb1a1c943e913a36006d2dcbfcabfee928
agamswaroop619 <agam.swaroop619@gmail.com>
2024-02-21 09:56:22 +0530

    add: FB page init

---
+++ src/app/fbconnect/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <main className="bg-blue-800 flex items-center justify-center h-screen"></main>
+  );
+}
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="bg-blue-800 flex items-center justify-center h-screen">
