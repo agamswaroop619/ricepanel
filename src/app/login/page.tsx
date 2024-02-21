061419ace87d7deedefb7cd2927093d192507eb6
agamswaroop619 <agam.swaroop619@gmail.com>
2024-02-21 09:46:07 +0530

    add: login page 1/3

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,62 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-blue-800 flex items-center justify-center h-screen"></main>
+    <main className="bg-blue-800 flex items-center justify-center h-screen">
+      <div className="bg-white rounded-lg p-8 max-w-sm mx-auto">
+        <h1 className="text-2xl font-semibold mb-6 text-black">
+          Login to your account
+        </h1>
+        <form>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="manoj@richpanel.com"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              placeholder="............"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-500 font-bold">
+              <input className="mr-2 leading-tight" type="checkbox" />
+              <span className="text-sm">Remember Me</span>
+            </label>
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+        <p className="text-center text-sm text-gray-600 mt-4">
+          New to MyApp?{" "}
+          <a href="#" className="text-blue-500 hover:text-blue-800">
+            Sign Up
+          </a>
+        </p>
+      </div>
+    </main>
   );
 }
