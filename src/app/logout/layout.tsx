748987ec1c1f17f941e8ab597848750ddeec04ff
agamswaroop619 <agam.swaroop619@gmail.com>
2024-02-21 10:05:22 +0530

    add: delete page init

--- src/app/logout/layout.tsx
+++ src/app/logout/layout.tsx
@@ -4,8 +4,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Logout",
-  description: "Delete Account/ Delete Integration",
+  title: "Connect",
+  description: "Connect to Facebook",
 };
 
 export default function RootLayout({
